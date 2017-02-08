1e50d68ec0ed8ded2d0967c9ee1f9e06b7b53f07
Tomas Janciga <tjanciga@cisco.com>
2017-02-08 09:28:41 -0800

    Improved filtering for PluginManager RPCs
    Change-Id: I23314208e93cecf9218fb07b015b5f527bc5dde5
    Signed-off-by: Tomas Janciga <tjanciga@cisco.com>

--- onem2m/onem2m-core/src/main/java/org/opendaylight/iotdm/onem2m/plugins/Onem2mPluginManagerUtils.java
+++ onem2m/onem2m-core/src/main/java/org/opendaylight/iotdm/onem2m/plugins/Onem2mPluginManagerUtils.java
@@ -8,6 +8,8 @@
 
 package org.opendaylight.iotdm.onem2m.plugins;
 
+import java.util.Map;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.iotdm.onem2mpluginmanager.rev161110.IotdmPluginFilters;
 import org.slf4j.Logger;
 import org.slf4j.helpers.MessageFormatter;
 
@@ -29,5 +31,73 @@ public final class Onem2mPluginManagerUtils {
         String msg = MessageFormatter.arrayFormat(format, args).getMessage();
         LOG.error("IotdmPlugin registration error: {}", msg);
         throw new IotdmPluginRegistrationException(msg);
+    }
+
+    /**
+     * Checks whether the plugin instance meets filtering criteria
+     * @param pluginName Plugin name filter
+     * @param instanceId Instance name filter
+     * @param plugin The plugin instance to be checked
+     * @return True if the plugin instance meets criteria, false otherwise.
+     */
+    public static boolean filterPlugin(final String pluginName, final String instanceId,
+                                       final IotdmPluginCommonInterface plugin) {
+        if (null == plugin) {
+            return false;
+        }
+
+        if (null != pluginName) {
+            if (! plugin.getPluginName().equals(pluginName)) {
+                return false;
+            }
+        }
+
+        if (null != instanceId) {
+            if (! plugin.getInstanceName().equals(instanceId)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Checks whether the plugin instance meets filtering criteria
+     * @param filters Filtering criteria
+     * @param plugin The plugin instance to be checked
+     * @return True if the plugin instance meets criteria, false otherwise.
+     */
+    public static boolean applyPluginFilters(final IotdmPluginFilters filters,
+                                             final IotdmPluginCommonInterface plugin) {
+        if (null == plugin) {
+            return false;
+        }
+
+        if (null == filters) {
+            // no any constraints
+            return true;
+        }
+
+        String pluginLoader = filters.getPluginLoaderName();
+        String pluginName = filters.getPluginName();
+        String pluginInstanceId = filters.getPluginInstanceName();
+
+        Map<String, IotdmPluginLoader> pluginLoaders = Onem2mPluginManager.getInstance().getMgrPluginLoaders();
+
+        IotdmPluginLoader loader = null;
+        if ((null != pluginLoader) && (! pluginLoader.isEmpty())) {
+            if (! pluginLoaders.containsKey(pluginLoader)) {
+                return false;
+            }
+
+            loader = pluginLoaders.get(pluginLoader);
+        }
+
+        if (null != loader) {
+            if (! loader.hasLoadedPlugin(plugin)) {
+                return false;
+            }
+        }
+
+        return filterPlugin(pluginName, pluginInstanceId, plugin);
     }
 }
