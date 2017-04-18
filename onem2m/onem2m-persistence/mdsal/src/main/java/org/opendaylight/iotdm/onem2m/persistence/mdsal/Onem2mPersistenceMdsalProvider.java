b3e3ccc369d656e4bd56a97e179b27e4f6d04564
Tomas Janciga <tjanciga@cisco.com>
2017-04-18 01:40:12 -0700

    Karaf4 migration - using Jetty 8
    Change-Id: I91f939f08bf59f0de7ca4c5b3e774a50f85b11b1
    Signed-off-by: Tomas Janciga <tjanciga@cisco.com>

--- onem2m/onem2m-persistence/mdsal/src/main/java/org/opendaylight/iotdm/onem2m/persistence/mdsal/Onem2mPersistenceMdsalProvider.java
+++ onem2m/onem2m-persistence/mdsal/src/main/java/org/opendaylight/iotdm/onem2m/persistence/mdsal/Onem2mPersistenceMdsalProvider.java
@@ -10,24 +10,26 @@ package org.opendaylight.iotdm.onem2m.persistence.mdsal;
 
 
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
-import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.ProviderContext;
-import org.opendaylight.controller.sal.binding.api.BindingAwareProvider;
 import org.opendaylight.iotdm.onem2m.core.Onem2mCoreProvider;
+import org.opendaylight.iotdm.onem2m.core.database.dao.factory.DaoResourceTreeFactoryRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Onem2mPersistenceMdsalProvider implements BindingAwareProvider, AutoCloseable {
+public class Onem2mPersistenceMdsalProvider implements AutoCloseable {
 
     private static final Logger LOG = LoggerFactory.getLogger(Onem2mPersistenceMdsalProvider.class);
+    private final DataBroker dataBroker;
+    private final DaoResourceTreeFactoryRegistry daoFactoryRegistry;
     private MDSALDaoResourceTreeFactory factory;
-    public Onem2mPersistenceMdsalProvider() {
+
+    public Onem2mPersistenceMdsalProvider(DataBroker dataBroker, DaoResourceTreeFactoryRegistry daoFactoryRegistry) {
+        this.dataBroker = dataBroker;
+        this.daoFactoryRegistry = daoFactoryRegistry;
     }
 
-    @Override
-    public void onSessionInitiated(ProviderContext session) {
-        DataBroker dataBroker = session.getSALService(DataBroker.class);
+    public void init() {
         this.factory = new MDSALDaoResourceTreeFactory(dataBroker);
-        Onem2mCoreProvider.getInstance().registerDaoPlugin(factory);
+        daoFactoryRegistry.registerDaoPlugin(factory);
         LOG.info("Onem2mPersistenceMdsalProvider Session Initiated");
     }
 
@@ -37,5 +39,4 @@ public class Onem2mPersistenceMdsalProvider implements BindingAwareProvider, Aut
         this.factory.close();
         LOG.info("Onem2mPersistenceMdsalProvider Closed");
     }
-
 }
